14787d8070c579b44290a06301f503cd9d67a03c
Antonio Rodriguez <102554647+antoniorodr@users.noreply.github.com>
2026-08-17 12:02:45 +0200

    feat(version): add versioninfo with flag

--- main.go
+++ main.go
@@ -3,8 +3,14 @@ Copyright © 2026 ANTONIO RODRIGUEZ <kontakt@antoniorodriguez.no>
 */
 package main
 
-import "github.com/antoniorodr/folkctl/cmd"
+import (
+	"flag"
+	"github.com/antoniorodr/folkctl/cmd"
+	"github.com/earthboundkid/versioninfo/v2"
+)
 
 func main() {
+	versioninfo.AddFlag(nil)
+	flag.Parse()
 	cmd.Execute()
 }
